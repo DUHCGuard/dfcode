b3826cf5e3be6401ad8235c7d24f033b44ae4f67
DUHCGuard <dantekupka@gmail.com>
2018-03-23 12:02:27 +0100

    Updated ping.js
    Removed not needed spaces

--- ping.js
+++ ping.js
@@ -1,16 +1,9 @@
-class DiamondFire extends Event {
-    Event (COMMAND) {
-        if (playerCommand === "ping") {
-            DiamondFire.apply.sendMessage("&a&l+ &fStarted generating.")
-            DiamondFire.apply.setChatTag("&8[&4Owner&8]");
-        };
-    };
-};
 
 class DiamondFire extends Event {
-  Event (COMMAND) {
-    if (playerCommand === "ping") {
-      DiamondFire.apply.sendMessage("Pong!")
-      }
+    Event (COMMAND) {
+      if (playerCommand === "ping") {
+        DiamondFire.apply.sendMessage("Pong!")
+        }
     }
- }
+}
+  
